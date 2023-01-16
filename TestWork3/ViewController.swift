e91736b88b80ba8768beb20baf3fbdf07e4b7bec
Ostap Artym <85745668+artym125@users.noreply.github.com>
2023-01-16 23:06:08 +0200

    Initial Commit

---
+++ TestWork3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestWork3
+//
+//  Created by Ostap Artym on 16.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
